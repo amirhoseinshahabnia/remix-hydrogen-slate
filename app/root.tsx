61911d77ed5b4551efb2885060a7c4cab0be0cd6
Amirhossein Shahabnia <amir@stevenslateaudio.com>
2024-06-20 11:55:58 -0700

    adds tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -16,6 +16,7 @@ import resetStyles from '~/styles/reset.css?url';
 import appStyles from '~/styles/app.css?url';
 import {PageLayout} from '~/components/PageLayout';
 import {FOOTER_QUERY, HEADER_QUERY} from '~/lib/fragments';
+import stylesheet from '~/tailwind.css?url';
 
 export type RootLoader = typeof loader;
 
@@ -53,6 +54,7 @@ export function links() {
       href: 'https://shop.app',
     },
     {rel: 'icon', type: 'image/svg+xml', href: favicon},
+    {rel: 'stylesheet', href: stylesheet},
   ];
 }
 
--- app/routes/($locale)._index.tsx
+++ app/routes/($locale)._index.tsx
@@ -94,7 +94,7 @@ function RecommendedProducts({
 }) {
   return (
     <div className="recommended-products">
-      <h2>Recommended Products</h2>
+      <h2 className="my-8">Recommended Products</h2>
       <Suspense fallback={<div>Loading...</div>}>
         <Await resolve={products}>
           {(response) => (
